1c1ed2bdaa336aa956014885bfba8634e0a99309
Love Erlandsson <loveerla@playgroundsquad.com>
2026-03-09 10:10:16 +0100

    Fixed new root

--- js/headerFooter.js
+++ js/headerFooter.js
@@ -1,6 +1,6 @@
 // ▼ ►
 
-const ROOT = (location.hostname == "localhost" || location.hostname == "127.0.0.1" || location.hostname == "") ? "/" : "/Portfolio-webpage/";
+const ROOT = (location.hostname == "localhost" || location.hostname == "127.0.0.1" || location.hostname == "") ? "/" : "/Portfolio/";
 
 const header = document.getElementById('header')
 const footer = document.getElementById('footer')
